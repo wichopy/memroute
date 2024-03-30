0a3023327ec7f2338d8ec0c3e042b484066b1b22
Will Chou <iam@willchou.dev>
2024-03-30 08:06:35 -0400

    dist

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = void 0;
+function add(a, b) {
+    return a + b;
+}
+exports.add = add;
+console.log(add(3, 5)); //output: 8
+exports.default = {
+    koa: {},
+    express: {},
+    node: {},
+};
